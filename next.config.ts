33f4b55bb1a06cd630989eb16d9c19c5ec36836a
yaelcambers <yaelcambers@gmail.com>
2026-03-09 15:45:32 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,1 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "framerusercontent.com"
-      }
-    ]
-  }
-};
-
-export default nextConfig;
